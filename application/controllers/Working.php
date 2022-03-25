85eb0a5e0b231d1abfd2ce297867801e50fb5841
Ahmad Harki Ramadhan <harkiramadhan@icloud.com>
2022-03-26 01:39:49 +0700

    updates

--- application/controllers/Working.php
+++ application/controllers/Working.php
@@ -18,6 +18,104 @@ class Working extends CI_Controller{
         $this->load->view('templates', $var);
     }
 
+    function create(){
+        $dataInsert = [
+            'kode' => $this->input->post('kode', TRUE),
+            'hari_kerja' => $this->input->post('hari_kerja', TRUE),
+            'jam_in' => $this->input->post('jam_in', TRUE),
+            'jam_out' => $this->input->post('jam_out', TRUE),
+        ];
+        $this->db->insert('jam_kerja', $dataInsert);
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Tambahkan");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Tambahkan");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function update($id){
+        $dataUpdate = [
+            'kode' => $this->input->post('kode', TRUE),
+            'hari_kerja' => $this->input->post('hari_kerja', TRUE),
+            'jam_in' => $this->input->post('jam_in', TRUE),
+            'jam_out' => $this->input->post('jam_out', TRUE),
+        ];
+        $this->db->where('id', $id)->update('jam_kerja', $dataUpdate);
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Simpan");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Simpan");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function delete($id){
+        $this->db->where('id', $id)->delete('jam_kerja');
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Hapus");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Hapus");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function edit($id){
+        $working = $this->db->get_where('jam_kerja', ['id' => $id])->row();
+        ?>
+            <div class="card card-plain">
+                <div class="card-header pb-0 text-left">
+                    <h5 class="font-weight-bolder">Edit Jam Kerja</h5>
+                </div>
+                <div class="card-body pb-0">
+                    <form action="<?= site_url('working/update/' . $id) ?>" role="form text-left" method="post">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label>kode <small class="text-danger">*</small></label>
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Kode" aria-label="Kode" name="kode" value="<?= $working->kode ?>" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Hari Kerja <small class="text-danger">*</small></label>
+                                    <select name="hari_kerja" class="form-control" id="exampleFormControlSelect1" required="">
+                                        <option value="" selected="" disabled="">- Pilih Hari Kerja</option>
+                                        <option <?= ($working->hari_kerja == "Senin") ? 'selected' : '' ?> value="Senin">Senin</option>
+                                        <option <?= ($working->hari_kerja == "Selasa") ? 'selected' : '' ?> value="Selasa">Selasa</option>
+                                        <option <?= ($working->hari_kerja == "Rabu") ? 'selected' : '' ?> value="Rabu">Rabu</option>
+                                        <option <?= ($working->hari_kerja == "Kamis") ? 'selected' : '' ?> value="Kamis">Kamis</option>
+                                        <option <?= ($working->hari_kerja == "Jum'at") ? 'selected' : '' ?> value="Jum'at">Jum</option>
+                                        <option <?= ($working->hari_kerja == "Sabtu") ? 'selected' : '' ?> value="Sabtu">Sabtu</option>
+                                        <option <?= ($working->hari_kerja == "Minggu") ? 'selected' : '' ?> value="Minggu">Minggu</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <label>Jam (In) <small class="text-danger">*) Format HH:MM</small></label>
+                                <div class="input-group mb-3">
+                                    <input type="time" class="form-control" placeholder="Jam (In)" aria-label="Jam (In)" name="jam_in" value="<?= $working->jam_in ?>" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <label>Jam (Out) <small class="text-danger">*) Format HH:MM</small></label>
+                                <div class="input-group mb-3">
+                                    <input type="time" class="form-control" placeholder="Jam (Out)" aria-label="Jam (Out)" name="jam_out" value="<?= $working->jam_out ?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-sm btn-round bg-success btn-lg w-100 mt-4 mb-0 text-white">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                    <button type="button" class="btn btn-sm btn-link btn-block  ml-auto" data-bs-dismiss="modal">Batal</button>
+                </div>
+            </div>
+        <?php
+    }
+
     function table(){
         $draw = intval($this->input->get("draw"));
         $start = intval($this->input->get("start"));
@@ -35,9 +133,22 @@ class Working extends CI_Controller{
                 '<strong>'.$row->jam_in.'</strong>',
                 '<strong>'.$row->jam_out.'</strong>',
                 '<div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-sm btn-round btn-info text-white px-3 mb-0 btn-edit" data-id="3"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</button>
-                    <a class="btn btn-sm btn-round btn-link text-danger px-3 mb-0" href="http://localhost/monitoring-ltq/kelas/delete/3"><i class="far fa-trash-alt" aria-hidden="true"></i></a>
-                </div>'
+                    <button type="button" class="btn btn-sm btn-round btn-info text-white px-3 mb-0" onclick="edit('.$row->id.')"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</button>
+                    <a class="btn btn-sm btn-round btn-link text-danger px-3 mb-0" href="'.site_url('working/delete/' . $row->id).'"><i class="far fa-trash-alt" aria-hidden="true"></i></a>
+                </div>
+                <script>
+                    function edit(id){
+                        $.ajax({
+                            url : "'.site_url('working/edit/').'" + id,
+                            type : "post",
+                            data : {id : id},
+                            success: function(res){
+                                $(".data-edit").html(res)
+                                $("#modalEdit").modal("show")
+                            }
+                        })
+                    }
+                </script>'
             ];
         }
                         
--- application/views/pages/working_hour.php
+++ application/views/pages/working_hour.php
@@ -7,7 +7,7 @@
                     <h5 class="mb-0"><strong>Jam Kerja</strong></h5>
                 </div>
                 <div class="col-lg-4 text-end">
-                    <button class="btn btn-sm btn-round bg-gradient-dark mb-0"><i class="fas fa-plus me-2"></i> Jam Kerja</button>
+                    <button type="button" class="btn btn-sm btn-round bg-gradient-dark mb-0" data-bs-toggle="modal" data-bs-target="#modalAdd"><i class="fas fa-plus me-2"></i> Jam Kerja</button>
                 </div>
             </div>
             <!-- <p class="text-sm mb-0">
@@ -33,3 +33,72 @@
     </div>
 </div>
 
+<!-- Modals -->
+<div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-body p-0">
+                <div class="card card-plain">
+                    <div class="card-header pb-0 text-left">
+                        <h5 class="font-weight-bolder">Tambah Jam Kerja</h5>
+                    </div>
+                    <div class="card-body pb-0">
+                        <form action="<?= site_url('working/create') ?>" role="form text-left" method="post">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <label>kode <small class="text-danger">*</small></label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Kode" aria-label="Kode" name="kode" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Hari Kerja <small class="text-danger">*</small></label>
+                                        <select name="hari_kerja" class="form-control" id="exampleFormControlSelect1" required="">
+                                            <option value="" selected="" disabled="">- Pilih Hari Kerja</option>
+                                            <option value="Senin">Senin</option>
+                                            <option value="Selasa">Selasa</option>
+                                            <option value="Rabu">Rabu</option>
+                                            <option value="Kamis">Kamis</option>
+                                            <option value="Jum'at">Jum</option>
+                                            <option value="Sabtu">Sabtu</option>
+                                            <option value="Minggu">Minggu</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <label>Jam (In) <small class="text-danger">*) Format HH:MM</small></label>
+                                    <div class="input-group mb-3">
+                                        <input type="time" class="form-control" placeholder="Jam (In)" aria-label="Jam (In)" name="jam_in" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <label>Jam (Out) <small class="text-danger">*) Format HH:MM</small></label>
+                                    <div class="input-group mb-3">
+                                        <input type="time" class="form-control" placeholder="Jam (Out)" aria-label="Jam (Out)" name="jam_out" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-sm btn-round bg-success btn-lg w-100 mt-4 mb-0 text-white">Tambahkan</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                        <button type="button" class="btn btn-sm btn-link btn-block  ml-auto" data-bs-dismiss="modal">Batal</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-body p-0 data-edit">
+              
+            </div>
+        </div>
+    </div>
+</div>
