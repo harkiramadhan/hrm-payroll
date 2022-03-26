36d2e23754aa973b8343e173783700b61bd354ec
Ahmad Harki Ramadhan <harkiramadhan@icloud.com>
2022-03-26 18:08:09 +0700

    updates

---
+++ application/controllers/Religion.php
@@ -0,0 +1,127 @@
+<?php
+class Religion extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        $this->load->model([
+            'M_Company',
+        ]);
+        if($this->session->userdata('masuk') != TRUE)
+            redirect('', 'refresh');
+    }
+
+    function index(){
+        $var = [
+            'title' => 'Agama',
+            'company' => $this->M_Company->getDefault(),
+            'page' => 'religion'
+        ];
+        $this->load->view('templates', $var);
+    }
+
+    function create(){
+        $dataInsert = [
+            'agama' => $this->input->post('agama', TRUE)
+        ];
+        $this->db->insert('agama', $dataInsert);
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Tambahkan");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Tambahkan");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function update($id){
+        $dataUpdate = [
+            'agama' => $this->input->post('agama', TRUE)
+        ];
+        $this->db->where('id', $id)->update('agama', $dataUpdate);
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Simpan");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Simpan");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function delete($id){
+        $this->db->where('id', $id)->delete('agama');
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Hapus");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Hapus");
+        }
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function edit($id){
+        $agama = $this->db->get_where('agama', ['id' => $id])->row();
+        ?>
+            <div class="card card-plain">
+                <div class="card-header pb-0 text-left">
+                    <h5 class="font-weight-bolder">Edit Agama</h5>
+                </div>
+                <div class="card-body pb-0">
+                    <form action="<?= site_url('religion/update/' . $id) ?>" role="form text-left" method="post">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label>Agama <small class="text-danger">*</small></label>
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Agama" aria-label="Agama" name="agama" value="<?= $agama->agama ?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-sm btn-round bg-success btn-lg w-100 mt-4 mb-0 text-white">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                    <button type="button" class="btn btn-sm btn-link btn-block  ml-auto" data-bs-dismiss="modal">Batal</button>
+                </div>
+            </div>
+        <?php
+    }
+
+    function table(){
+        $draw = intval($this->input->get("draw"));
+        $start = intval($this->input->get("start"));
+        $length = intval($this->input->get("length"));
+
+        $get = $this->db->get('agama');
+
+        $data = array();
+        $no = 1;
+        foreach($get->result() as $row){
+            $data[] = [
+                $no++,
+                '<strong>'.$row->agama.'</strong>',
+                '<div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-sm btn-round btn-info text-white px-3 mb-0" onclick="edit('.$row->id.')"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</button>
+                    <a class="btn btn-sm btn-round btn-link text-danger px-3 mb-0" href="'.site_url('religion/delete/' . $row->id).'"><i class="far fa-trash-alt" aria-hidden="true"></i></a>
+                </div>
+                <script>
+                    function edit(id){
+                        $.ajax({
+                            url : "'.site_url('religion/edit/').'" + id,
+                            type : "post",
+                            data : {id : id},
+                            success: function(res){
+                                $(".data-edit").html(res)
+                                $("#modalEdit").modal("show")
+                            }
+                        })
+                    }
+                </script>'
+            ];
+        }
+                        
+        $output = [
+            "draw"              => $draw,
+            "recordsTotal"      => $get->num_rows(),
+            "recordsFiltered"   => $get->num_rows(),
+            "data"              => $data
+        ];
+        $this->output->set_content_type('application/json')->set_output(json_encode($output));
+    }
+}
--- application/views/templates.php
+++ application/views/templates.php
@@ -60,7 +60,7 @@
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         </div>
-        <div class="collapse navbar-collapse w-100 h-100 mt-n6" id="sidenav-collapse-main">
+        <div class="collapse navbar-collapse w-100 h-auto mt-n6" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <?php foreach($menu->result() as $row){ ?>
                 <?php if($row->dropdown == 'f'): ?>
