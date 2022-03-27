b770d5a90759234d4c0a6c7b3b9c0086b4d63602
Ahmad Harki Ramadhan <harkiramadhan@icloud.com>
2022-03-27 20:11:36 +0700

    updates

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -36,6 +36,38 @@ class Employee extends CI_Controller{
         $this->load->view('templates', $var);
     }
 
+    function edit($id){
+        $pegawai = $this->db->select('p.*, a.agama, pd.jenjang, c.company, j.jabatan, d.divisi, dp.departement, u.unit')
+                        ->from('pegawai p')
+                        ->join('agama a', 'p.agama_id = a.id', "LEFT")
+                        ->join('jenjang_pendidikan pd', 'p.pendidikan_id = pd.id', "LEFT")
+                        ->join('company c', 'p.company_id = c.id', "LEFT")
+                        ->join('jabatan j', 'p.jabatan_id = j.id', "LEFT")
+                        ->join('divisi d', 'p.divisi_id = d.id', "LEFT")
+                        ->join('departement dp', 'p.dept_id = dp.id', "LEFT")
+                        ->join('unit u', 'p.unit_id = u.id', "LEFT")
+                        ->where([
+                            'p.id' => $id
+                        ])->get()->row();
+        $var = [
+            'title' => 'Edit Pegawai ' . $pegawai->nama,
+            'pegawai' => $pegawai,
+            'company' => $this->M_Company->getDefault(),
+            'agama' => $this->db->get('agama'),
+            'pendidikan' => $this->db->get('jenjang_pendidikan'),
+            'jabatan' => $this->db->get('jabatan'),
+            'companys' => $this->db->get('company'),
+            'divisi' => $this->db->get('divisi'),
+            'departement' => $this->db->get_where('departement', ['divisi_id' => $pegawai->divisi_id]),
+            'unit' => $this->db->get_where('unit', ['dept_id' => $pegawai->dept_id]),
+            'page' => 'edit_employee',
+            'ajax' => [
+                'employee'
+            ]
+        ];
+        $this->load->view('templates', $var);
+    }
+
     function create(){
         $this->form_validation->set_rules('nik', 'NIK', 'is_unique[pegawai.nik]', [
             'is_unique' => '<strong>NIK Sudah Tersedia</strong>'
@@ -131,22 +163,9 @@ class Employee extends CI_Controller{
                 '<p class="text-center"><strong>'.$row->unit.'</strong></p>',
                 '<p class="text-center"><strong>'.date('Y-m-d H:i:s', strtotime($row->updated_at)).'</strong></p>',
                 '<div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-sm btn-round btn-info text-white px-3 mb-0" onclick="edit('.$row->id.')"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</button>
-                    <a class="btn btn-sm btn-round btn-link text-danger px-3 mb-0" href="'.site_url('holidays/delete/' . $row->id).'"><i class="far fa-trash-alt" aria-hidden="true"></i></a>
-                </div>
-                <script>
-                    function edit(id){
-                        $.ajax({
-                            url : "'.site_url('holidays/edit/').'" + id,
-                            type : "post",
-                            data : {id : id},
-                            success: function(res){
-                                $(".data-edit").html(res)
-                                $("#modalEdit").modal("show")
-                            }
-                        })
-                    }
-                </script>'
+                    <a class="btn btn-sm btn-round btn-info text-white px-3 mb-0" href="'.site_url('employee/' . $row->id).'"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</a>
+                    <a class="btn btn-sm btn-round btn-link text-danger px-3 mb-0" href="'.site_url('employee/delete/' . $row->id).'"><i class="far fa-trash-alt" aria-hidden="true"></i></a>
+                </div>'
             ];
         }
                         
