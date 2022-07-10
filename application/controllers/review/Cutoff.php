2271a1b64868bfe795e780323d0a80bab61f4830
harkiramadhan <harkiramadhan@gmail.com>
2022-07-10 15:17:01 +0700

    updates

--- application/controllers/review/Cutoff.php
+++ application/controllers/review/Cutoff.php
@@ -34,6 +34,135 @@ class Cutoff extends CI_Controller{
         $this->load->view('templates', $var);
     }
 
+    function update($id){
+        $datas = [
+            'hari_efektif' => $this->input->post('hari_efektif', TRUE),
+            'total_hadir' => $this->input->post('total_hadir', TRUE),
+            'izin' => $this->input->post('izin', TRUE),
+            'sakit' => $this->input->post('sakit', TRUE),
+            'alpa' => $this->input->post('alpa', TRUE),
+            'hari_terlambat' => $this->input->post('hari_terlambat', TRUE),
+            'menit_terlambat' => $this->input->post('menit_terlambat', TRUE),
+            'hari_lembur' => $this->input->post('hari_lembur', TRUE),
+            'menit_lembur' => $this->input->post('menit_lembur', TRUE),
+            'shift' => $this->input->post('shift', TRUE)
+        ];
+        $this->db->where('id', $id)->update('summary', $datas);
+        if($this->db->affected_rows() > 0){
+            $this->session->set_flashdata('success', "Data Berhasil Di Simpan");
+        }else{
+            $this->session->set_flashdata('error', "Data Gagal Di Simpan");
+        }
+
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    function edit($id){
+        $summary = $this->db->select('s.*, p.nama')
+                            ->from('summary s')
+                            ->join('pegawai p', 's.nip = p.nik')
+                            ->where('s.id', $id)->get()->row();
+        
+        ?>
+            <div class="card card-plain">
+                <div class="card-header pb-0 text-left">
+                    <h5 class="font-weight-bolder">Edit Summary</h5>
+                </div>
+                <div class="card-body pb-0">
+                    <form action="<?= site_url('review/cutoff/update/' . $id) ?>" role="form text-left" method="post">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <label>NIP</label>
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="NIP" aria-label="NIP" name="nip" value="<?= $summary->nip ?>" disabled>
+                                </div>
+                            </div>
+                            <div class="col-lg-9">
+                                <label>Nama</label>
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Nama" aria-label="Nama" name="nama" value="<?= $summary->nama ?>" disabled>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Hari Efektif</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Hari Efektif" aria-label="Hari Efektif" name="hari_efektif" value="<?= $summary->hari_efektif ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Hadir (Hari)</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Hadir" aria-label="Total Hadir" name="total_hadir" value="<?= $summary->total_hadir ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Izin</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Izin" aria-label="Total Izin" name="izin" value="<?= $summary->izin ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Sakit</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Sakit" aria-label="Total Sakit" name="sakit" value="<?= $summary->sakit ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Alpa</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Alpa" aria-label="Total Alpa" name="alpa" value="<?= $summary->alpa ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Terlambat (Hari)</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Terlambat" aria-label="Total Terlambat" name="hari_terlambat" value="<?= $summary->hari_terlambat ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Terlambat (Menit)</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Terlambat" aria-label="Total Terlambat" name="menit_terlambat" value="<?= $summary->menit_terlambat ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Lembur (Hari)</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Lembur" aria-label="Total Lembur" name="hari_lembur" value="<?= $summary->hari_lembur ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <label>Total Lembur (Menit)</label>
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" placeholder="Total Lembur" aria-label="Total Lembur" name="menit_lembur" value="<?= $summary->menit_lembur ?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-9">
+                                <div class="form-group">
+                                    <label for="select-div">Shift</label>
+                                    <select name="shift" class="form-control">
+                                        <option value="" selected="" disabled="">- Pilih Shift</option>
+                                        <?php 
+                                            $shift = $this->db->get_where('shift', ['company_id' => $this->companyid, 'status' => 't']);
+                                            foreach($shift->result() as $d){ ?>
+                                            <option value="<?= $d->keterangan ?>" <?= ($d->keterangan == $summary->shift) ? 'selected' : '' ?> ><?= $d->keterangan ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-sm btn-round bg-success btn-lg w-100 mt-4 me-4 ml-4 mb-0 text-white">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                    <button type="button" class="btn btn-sm btn-link btn-block  ml-auto" data-bs-dismiss="modal">Batal</button>
+                </div>
+            </div>
+        <?php
+    }
+
     function table(){
         $draw = intval($this->input->get("draw"));
         $start = intval($this->input->get("start"));
@@ -141,7 +270,7 @@ class Cutoff extends CI_Controller{
                             <td class="text-center"><strong><?= $row->menit_lembur." Menit" ?></strong></td>
                             <td><strong><?= $row->shift ?></strong></td>
                             <td>
-                                
+                                <button type="button" class="btn btn-sm btn-round btn-info text-white px-3 mb-0" onclick="edit(<?= $row->id ?>)"><i class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Edit</button>
                             </td>
                         </tr>
                         <?php } ?>
@@ -150,6 +279,18 @@ class Cutoff extends CI_Controller{
             </div>
 
             <script>
+                function edit(id){
+                    $.ajax({
+                        url: '<?= site_url('review/cutoff/edit/') ?>' + id,
+                        type: 'get',
+                        data: {id : id},
+                        success: function(res){
+                            $('.data-edit').html(res)
+                            $('#modalEdit').modal('show')
+                        }
+                    })
+                }
+
                 $('#cabang_id').change(function(){
                     var cabangid = $(this).val()
                     var cutoffid = '<?= $cutoffid ?>'
